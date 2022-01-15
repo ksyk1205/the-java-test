088547bc247a20ce7069f3f723c488c1016fcd44
Gwak seyeon <ksyk1205@naver.com>
2022-01-16 00:20:53 +0900

    JUnit 5 조건에 따라 테스트 진행하기

--- src/test/java/me/whiteship/thejavatest/StudyTest.java
+++ src/test/java/me/whiteship/thejavatest/StudyTest.java
@@ -1,36 +1,37 @@
 package me.whiteship.thejavatest;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.condition.*;
 
 import java.time.Duration;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.assumingThat;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class StudyTest {
     @Test
     @DisplayName("스터디 만들기 \uD83D\uDE31")
+    @EnabledIfEnvironmentVariable(named = "TEST_ENV", matches = "LOCAL")
     void create_new_study(){
-        assertTimeout(Duration.ofMillis(100), () -> {
-            new Study(10);
-            Thread.sleep(300);
+        String test_env = System.getenv("TEST_ENV");
+        //System.out.println(test_env);
+        //assertTrue("LOCAL".equalsIgnoreCase(test_env));
+
+        assumingThat("LOCAL".equalsIgnoreCase(test_env),()->{
+            Study actual = new Study(100);
+            assertThat(actual.getLimit()).isGreaterThan(0);
         });
 
-//        IllegalArgumentException exception =
-//                assertThrows(IllegalArgumentException.class, () -> new Study(-10));
-//
-//        assertEquals("limit은 0보다 커야 한다.",exception.getMessage());
+        assumingThat("keesun".equalsIgnoreCase(test_env),()->{
+            Study actual = new Study(10);
+            assertThat(actual.getLimit()).isGreaterThan(0);
+        });
 
-//        Study study = new Study(-10);
-//        assertAll(
-//                ()->  assertNotNull(study),
-//                ()->assertEquals(StudyStatus.DRAFT, study.getStatus(),
-//                        () ->"스터디를 처음 만들면 상태값이"+StudyStatus.DRAFT+"여야 한다."),
-//                ()->assertTrue(study.getLimit() >0 ,"스터디는 최대 참석 가능 인원은 0보다 커야 한다")
-//        );
     }
     @Test
-    //@Disabled
+    @DisabledOnOs(OS.MAC)
     void create_new_study_again(){
         System.out.println("create1");
     }
