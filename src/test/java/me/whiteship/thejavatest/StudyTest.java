ae90e07efb83a93b5cbadd834cff2ad31a28b814
Gwak seyeon <ksyk1205@naver.com>
2022-01-23 16:26:18 +0900

    JUnit 5 테스트 반복, 인스턴스, 순서, 마이그레이션

--- src/test/java/me/whiteship/thejavatest/StudyTest.java
+++ src/test/java/me/whiteship/thejavatest/StudyTest.java
@@ -2,6 +2,18 @@ package me.whiteship.thejavatest;
 
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.condition.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.ParameterContext;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.aggregator.AggregateWith;
+import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregationException;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregator;
+import org.junit.jupiter.params.converter.ArgumentConversionException;
+import org.junit.jupiter.params.converter.ConvertWith;
+import org.junit.jupiter.params.converter.SimpleArgumentConverter;
+import org.junit.jupiter.params.provider.*;
 
 import java.time.Duration;
 
@@ -9,31 +21,55 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assumptions.assumingThat;
 
-@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class StudyTest {
-    @Test
-    @DisplayName("스터디 만들기 \uD83D\uDE31")
-    @EnabledIfEnvironmentVariable(named = "TEST_ENV", matches = "LOCAL")
+
+    @RegisterExtension
+    static FindSlowTestExtension findSlowTestExtension =
+            new FindSlowTestExtension(1000L);
+
+    @Order(1)
+    @FastTest
+    @DisplayName("스터디 만들기 fast")
     void create_new_study(){
-        String test_env = System.getenv("TEST_ENV");
-        //System.out.println(test_env);
-        //assertTrue("LOCAL".equalsIgnoreCase(test_env));
-
-        assumingThat("LOCAL".equalsIgnoreCase(test_env),()->{
-            Study actual = new Study(100);
-            assertThat(actual.getLimit()).isGreaterThan(0);
-        });
-
-        assumingThat("keesun".equalsIgnoreCase(test_env),()->{
-            Study actual = new Study(10);
-            assertThat(actual.getLimit()).isGreaterThan(0);
-        });
-
+        Study actual = new Study(100);
+        assertThat(actual.getLimit()).isGreaterThan(0);
     }
-    @Test
-    @DisabledOnOs(OS.MAC)
+    @Order(2)
+    @SlowTest
+    @DisplayName("스터디 만들기 slow")
     void create_new_study_again(){
         System.out.println("create1");
+    }
+
+    @Order(3)
+    @DisplayName("스터디 만들기")
+    @RepeatedTest(value = 10, name = "{displayName}, {currentRepetition}/{totalRepetitions}")
+    void repeatTest(RepetitionInfo repetitionInfo){
+        System.out.println("test" + repetitionInfo.getCurrentRepetition()+"/"+
+                repetitionInfo.getTotalRepetitions());
+    }
+    @Order(4)
+    @DisplayName("스터디 만들기")
+    @ParameterizedTest(name = "{index} {displayName} message={0}")
+    @CsvSource({"10,'자바 스터디'", "20, '스프링'"})
+    void parameterizedTest(@AggregateWith(StudyAggregator.class) Study study ){
+        System.out.println(study);
+    }
+    static class StudyAggregator implements ArgumentsAggregator{
+        @Override
+        public Object aggregateArguments(ArgumentsAccessor accessor, ParameterContext parameterContext) throws ArgumentsAggregationException {
+            return new Study(accessor.getInteger(0), accessor.getString(1));
+        }
+    }
+
+    static class StudyConverter extends SimpleArgumentConverter{
+        @Override
+        protected Object convert(Object source, Class<?> targetType) throws ArgumentConversionException {
+            assertEquals(Study.class, targetType,"can only convert to study");
+            return new Study(Integer.parseInt(source.toString()));
+        }
     }
 
     @BeforeAll
